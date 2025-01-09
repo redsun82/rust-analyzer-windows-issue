2e76f9a498cadc66b39da41353619a1babd0049e
Paolo Tranquilli <redsun82@gihub.com>
2025-01-09 09:47:45 +0100

    path

--- tests/load.rs
+++ tests/load.rs
@@ -1,17 +1,19 @@
-use std::path::PathBuf;
 use ra_ap_hir::Semantics;
 use ra_ap_load_cargo::{load_workspace_at, LoadCargoConfig, ProcMacroServerChoice};
-use ra_ap_paths::AbsPathBuf;
+use ra_ap_paths::{AbsPathBuf, Utf8PathBuf};
 use ra_ap_project_model::{CargoConfig, RustLibSource};
 use ra_ap_vfs::VfsPath;
+use std::path::PathBuf;
 
 #[test]
 fn load() {
-    let data_dir = PathBuf::from(concat!(env!("CARGO_MANIFEST_DIR"), "/tests/data"));
-    let manifest = data_dir.join("lib/Cargo.toml");
-    let cargo_config = CargoConfig{
-        sysroot: Some(RustLibSource::Discover),
-        ..Default::default()
+    let lib_dir = PathBuf::from(concat!(env!("CARGO_MANIFEST_DIR"), "/tests/data/lib"))
+        .canonicalize()
+        .expect("canonicalize");
+    let manifest = lib_dir.join("Cargo.toml");
+    let cargo_config = CargoConfig {
+        // sysroot: Some(RustLibSource::Discover),
+        ..CargoConfig::default()
     };
     let load_cargo_config = LoadCargoConfig {
         load_out_dirs_from_check: true,
@@ -19,10 +21,16 @@ fn load() {
         prefill_caches: false,
     };
     let progress = |s| println!("{}", s);
-    let (db, vfs, _) = load_workspace_at(&manifest, &cargo_config, &load_cargo_config, &progress).expect("load_workspace_at");
-    let lib_path = AbsPathBuf::try_from(data_dir.join("lib/src/lib.rs").to_string_lossy().as_ref()).expect("lib_path");
+    let (db, vfs, _) = load_workspace_at(&manifest, &cargo_config, &load_cargo_config, &progress)
+        .expect("load_workspace_at");
+    let lib_path = Utf8PathBuf::from_path_buf(lib_dir.join("src/lib.rs"))
+        .ok()
+        .and_then(|p| AbsPathBuf::try_from(p).ok())
+        .expect("lib_path");
     let lib_file_id = vfs.file_id(&VfsPath::from(lib_path)).expect("file_id");
     let sema = Semantics::new(&db);
-    let module = sema.file_to_module_def(lib_file_id).expect("file_to_module_def");
+    let module = sema
+        .file_to_module_def(lib_file_id)
+        .expect("file_to_module_def");
     _ = module;
 }
